4beb5052f0fb858b05a103d65d26917e7ef1dc7e
Igor Malyga <igormalyga@gmail.com>
2020-04-30 18:37:16 +0300

    Remove commented code

--- server/server/schema.py
+++ server/server/schema.py
@@ -1,46 +1,5 @@
 import graphene
-from graphene_django import DjangoObjectType
 from graphene_file_upload.scalars import Upload
-
-
-class CUDNewsBoard(graphene.Mutation):
-    newsboard = graphene.String()
-
-    class Arguments:  # other arguments up here...
-        files = Upload()
-
-    def mutate(self, info, files=None):
-        # user = info.context.user
-
-        # if not user.is_staff:
-        #     raise Exception('Admin permission is required!')
-
-        # post = NewsBoard(user=user, title=title, content=content)
-        # post.save()
-
-        if files:
-            for file in files:
-                print('A'*50, flush=True)
-                print(file, flush=True)
-                # postimage = NewsBoardImage(post=post, image=None)
-                # postimage.save()
-
-                # image_obj = Image.open(file)
-                # # resize image
-                # image_obj = resizeimage.resize_width(
-                #     image_obj, 800, validate=False)
-
-                # new_image_io = BytesIO()
-                # image_obj.save(new_image_io, image_obj.format)
-
-                # temp_name = file.name
-
-                # postimage.image.save(
-                #     temp_name,
-                #     content=ContentFile(new_image_io.getvalue())
-                # )
-
-        return CUDNewsBoard(newsboard='Hello')
 
 
 class UploadMutation(graphene.Mutation):
@@ -50,7 +9,6 @@ class UploadMutation(graphene.Mutation):
     success = graphene.Boolean()
 
     def mutate(self, info, file, **kwargs):
-        # do something with your file
         print('A'*50, flush=True)
         print(file, flush=True)
         print(type(file), flush=True)
@@ -66,7 +24,6 @@ class Query(graphene.ObjectType):
 
 
 class Mutation(graphene.ObjectType):
-    post_new = CUDNewsBoard.Field()
     upload = UploadMutation.Field()
 
 
